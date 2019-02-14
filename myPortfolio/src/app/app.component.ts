afd0cb91807cac0591aa93b140215fdecc059ffd
Giuseppe Cuce <giuseppecuce.og@gmail.com>
2019-02-14 22:38:43 +0200

    Custom Cursor Complete

--- myPortfolio/src/app/app.component.ts
+++ myPortfolio/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Giuseppefolio';
+
+    onClick() {
+  	console.log("Clicked");
+  }
 }
--- myPortfolio/src/app/profile/profile.component.ts
+++ myPortfolio/src/app/profile/profile.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit, AfterViewInit, ViewChild, HostListener } from '@angu
 })
 export class ProfileComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('cursorList') cursorList;
   @ViewChild('cursor') cursor; 
   @ViewChild('cursorOne') cursorOne;  
-  @ViewChild('cursorTwo') cursorTwo;  
+  @ViewChild('cursorTwo') cursorTwo;
+  @ViewChild('cursorThree') cursorThree;  
   x: number;
   y: number;
   pos;
@@ -26,6 +28,10 @@ export class ProfileComponent implements OnInit, AfterViewInit {
   	this.cursorOne.nativeElement.style.display = "none";
   	this.cursorTwo.nativeElement.style.position = "absolute";
   	this.cursorTwo.nativeElement.style.display = "none";
+    this.cursorThree.nativeElement.style.position = "absolute";
+  	this.cursorThree.nativeElement.style.display = "none";  	
+  	this.cursorList.nativeElement.style.position = "absolute";
+  	this.cursorList.nativeElement.style.display = "none";	
   }
 
   @HostListener('mousemove') trackCursor() {
@@ -33,13 +39,30 @@ export class ProfileComponent implements OnInit, AfterViewInit {
   	this.x = this.pos.pageX;
   	this.y = this.pos.pageY;
   	this.cursor.nativeElement.style.display = "block";
-  	this.cursor.nativeElement.style.top = (this.y - 12) + "px";
-  	this.cursor.nativeElement.style.left = (this.x - 46) + "px";
   	this.cursorOne.nativeElement.style.display = "block";
-  	this.cursorOne.nativeElement.style.top = (this.y - 19) + "px";
-  	this.cursorOne.nativeElement.style.left = (this.x - 53) + "px";
+  	this.cursorOne.nativeElement.style.marginLeft = "-5px";
+  	this.cursorOne.nativeElement.style.marginTop = "-5px";
   	this.cursorTwo.nativeElement.style.display = "block";
-  	this.cursorTwo.nativeElement.style.top = (this.y - 7) + "px";
-  	this.cursorTwo.nativeElement.style.left = (this.x - 40) + "px";
+  	this.cursorTwo.nativeElement.style.marginLeft = "2px";
+  	this.cursorTwo.nativeElement.style.marginTop = "2.5px";
+  	this.cursorThree.nativeElement.style.display = "block";
+  	this.cursorThree.nativeElement.style.marginLeft = "-9px";
+  	this.cursorThree.nativeElement.style.marginTop = "-9px";
+  	this.cursorList.nativeElement.style.display = "block";
+  	this.cursorList.nativeElement.style.top = (this.y - 10) + "px";
+  	this.cursorList.nativeElement.style.left = (this.x - 41) + "px";
+  }
+
+  @HostListener('click') speedUp() {
+  	this.cursor.nativeElement.classList.add('speedUp');
+  	this.cursorOne.nativeElement.classList.add('speedUpReverse');
+  	this.cursorTwo.nativeElement.classList.add('expand');
+  	this.cursorThree.nativeElement.classList.add('speedUp');
+  	setTimeout(() => {
+		this.cursor.nativeElement.classList.remove('speedUp');
+	  	this.cursorOne.nativeElement.classList.remove('speedUpReverse');
+	  	this.cursorTwo.nativeElement.classList.remove('expand');
+	  	this.cursorThree.nativeElement.classList.remove('speedUp');
+  	}, 400)
   }
 }
