60970b8735496b94f5f05c96f8017a548dc9c385
Giuseppe Cuce <giuseppecuce.og@gmail.com>
2019-03-07 16:07:21 +0200

    improved works page

--- myPortfolio/src/app/shared/infos.ts
+++ myPortfolio/src/app/shared/infos.ts
@@ -2,58 +2,58 @@ import { Info } from './info';
 
 export const INFOS: Info[] = [
 	{
-		image: '../../assets/images/image5.png',
+		image: 'five',
 		title: 'Seat Selection Finnair Web Page',
 		description: 'Restyling of the existing Seat Selection page',
 		technologies: 'Mustache, Sass, Javascript',
 		company: 'Frantic'
 	},
 	{
-		image: '../../assets/images/image6.png',
+		image: 'six',
 		title: 'SuomiSelostaa Finnair Web Page',
 		description: 'Landing page created for Finnair',
 		technologies: 'Mustache, Sass, Javascript',
 		company: 'Frantic'
 	},
 	{
-		image: '../../assets/images/image3.png',
+		image: 'three',
 		title: 'Flappy Game',
 		description: 'Browser flappy game like, press Key up and Key down to interact with the game, (Yes you must play it!)',
 		technologies: 'Html, Css, Javascript',
 		company: 'Drama Queen Communications'
 	},
 	{
-		image: '../../assets/images/image1.png',
+		image: 'one',
 		title: 'Benny\'s Trading',
 		description: 'Entire Front End side built by me',
 		technologies: 'Html, Css, Javascript',
 		company: 'Drama Queen Communications'
 	},
 	{
-		image: '../../assets/images/image4.png',
+		image: 'four',
 		title: 'Page Created with Angular 1',
 		description: 'Page built with Angular by fetching data from the Marvel API',
 		technologies: 'Html, Css, Angular 1',
 		company: 'None'
 	},
 	{
-		image: '../../assets/images/image7.png',
+		image: 'seven',
 		title: 'Map of Italy with D3',
 		description: 'Exploring the potential of D3 in Data Visualization, in this map, every circle corresponds to a city, the bigger the circle, more people live in that specific city',
 		technologies: 'Html, Css, D3',
 		company: 'None'
 	},
 	{
-		image: '../../assets/images/image2.png',
+		image: 'two',
 		title: 'Web Page created by studying with Codecademy',
-		description: 'Studying Front End Technology with Codecademy',
+		description: 'Studying Front End Technology',
 		technologies: 'Html, Css, Javascript',
 		company: 'None'
 	},
 	{
-		image: '../../assets/images/image8.png',
+		image: 'eight',
 		title: 'Web Page created by studying with Codecademy',
-		description: 'Studying Front End Technology with Codecademy',
+		description: 'Studying Front End Technology',
 		technologies: 'Html, Css, Javascript',
 		company: 'None'
 
